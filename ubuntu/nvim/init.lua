5aa1c82f4929cda80e8b44eb569fc9e561c5135f
泰友 <cuichengxu.ccx@antgroup.com>
2023-12-29 03:26:47 +0000

    feat: replace init.vim with init.lua
    Signed-off-by: 泰友 <cuichengxu.ccx@antgroup.com>

---
+++ ubuntu/nvim/init.lua
@@ -0,0 +1,46 @@
+-- trival stuff
+vim.o.number = true
+vim.o.fileencodings = "utf-8,ucs-bom,gb18030,gbk,gb2312,cp936"
+vim.o.termencoding = "utf-8"
+vim.o.encoding = "utf-8"
+vim.o.cursorline = true
+vim.o.showmatch = true
+vim.o.expandtab = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.wrap = true
+vim.o.linebreak = true
+vim.o.wrapmargin = 2
+vim.o.scrolloff = 5
+vim.o.ruler = true
+vim.o.history = 1000
+vim.o.listchars = "tab:»■,trail:■"
+vim.o.maxmempattern = 2000
+vim.o.switchbuf = "usetab,newtab"
+vim.o.cursorcolumn = true
+vim.o.colorcolumn = "80"
+vim.o.bomb = false -- remove BOM of UTF8-file
+
+-- autocommand
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "rust",
+    callback = function() vim.opt_local.foldmethod = "syntax" end
+})
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "go",
+    callback = function() vim.opt_local.foldmethod = "syntax" end
+})
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "python",
+    callback = function() vim.opt_local.foldmethod = "indent" end
+})
+vim.api.nvim_create_autocmd("User", {
+    pattern = "CocStatusChange",
+    command = "redraws"
+})
+
+-- plugins installation
+require("plugins")
+
+-- coc configuration
+require("coc")
---
+++ ubuntu/nvim/lua/plugins.lua
@@ -0,0 +1,7 @@
+ vim.cmd [[
+     call plug#begin('~/.config/nvim/plugged')
+         Plug 'preservim/nerdtree'
+         Plug 'numToStr/Comment.nvim'
+         Plug 'neoclide/coc.nvim', {'branch': 'release'}
+     call plug#end()
+ ]]

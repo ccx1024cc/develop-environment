54973de90f37a602510cd96a61c7ea5d6b2bcd47
cuichengxu.morgan <cuichengxu.morgan@bytedance.com>
2024-05-21 10:19:14 +0800

    feat: show absolutely file path for neovim statusline
    Change-Id: Ie18c4e6a56c26d3ef93dd7c2fa2dbe1b1c9da505
    Signed-off-by: cuichengxu.morgan <cuichengxu.morgan@bytedance.com>

--- ubuntu/nvim/lua/statusline.lua
+++ ubuntu/nvim/lua/statusline.lua
@@ -1,6 +1,6 @@
 vim.g.lightline = {
   active = {
-    left = {{'mode', 'paste'}, {'cocstatus', 'currentfunction', 'readonly', 'filename', 'modified'}}
+    left = {{'mode', 'paste'}, {'cocstatus', 'currentfunction', 'readonly', 'absolutepath', 'modified'}}
   },
   component_function = {
     cocstatus = 'coc#status',
